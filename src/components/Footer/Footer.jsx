0b80d6cdbf515507da1f96c38d9bbb7dda4e145e
Robson Rosa <contatorobsonrosa@gmail.com>
2022-04-17 19:07:39 -0300

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,9 @@
 import './Footer.css'
 import React from 'react'
 
-export default props =>
-    <footer className="footer">
+export default props => <footer className="footer">
         <span>
             Desenvolvido com <i className="fa fa-heart text-danger"></i> por
-            <strong> <a blank href="http://linkedin.com/in/robsonrosajr">Robson Rosa</a></strong>
+            <strong> <a target="_blank" href="http://linkedin.com/in/robsonrosajr">Robson Rosa</a></strong>
         </span>
     </footer>
